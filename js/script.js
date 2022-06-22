2b5f489f92fb25f0ee14f19a8ef7ca4b8444c30a
Katy Trop <etropkina@yandex.ru>
2022-06-22 11:56:25 +0300

    feat: add burger

--- js/script.js
+++ js/script.js
@@ -100,3 +100,16 @@ const closeModal = () => {
     modal.classList.remove('active');
 };
 closeBtn.addEventListener('click', closeModal);
+
+// burger
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.promo__menu');
+const span =  document.querySelectorAll('.hamburger span');
+
+const showMenu = () => {
+    menu.classList.toggle('active');
+	span.forEach(item => {
+		item.classList.toggle('active');
+	});
+};
+hamburger.addEventListener('click', showMenu);
